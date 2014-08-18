966c869ac58fe04f2bf2149ff01c2510d21903d7
Carl Winbäck <c@tunnel53.net>
2014-08-18 11:50:26 +0200

    Moved xor_singlechar and xor_find_singlechar_key to the util module.

---
+++ libpals/util.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from operator import itemgetter
+
+character_frequency = {
+    'e': 27,
+    't': 26,
+    'a': 25,
+    'o': 24,
+    'i': 23,
+    'n': 22,
+    's': 21,
+    'r': 20,
+    'h': 19,
+    'l': 18,
+    'd': 17,
+    'c': 16,
+    'u': 15,
+    'm': 14,
+    'f': 13,
+    'p': 12,
+    'g': 11,
+    'w': 10,
+    'y': 9,
+    'b': 8,
+    'v': 7,
+    'k': 6,
+    'x': 5,
+    ' ': 4,
+    'j': 3,
+    'q': 2,
+    'z': 1
+}
+
+
+def xor_singlechar(input_bytes, key_value):
+    output = b''
+
+    for char in input_bytes:
+        output += bytes([char ^ key_value])
+
+    return output
+
+
+def xor_find_singlechar_key(ciphertext):
+    candidates = list()
+
+    for key_candidate in range(256):
+        total_score = 0
+        decoded_input = xor_singlechar(ciphertext, key_candidate)
+
+        for byte in decoded_input:
+            char_score = character_frequency.get(chr(byte), 0)
+            total_score += char_score
+        candidates.append((key_candidate, total_score))
+
+    return max(candidates, key=itemgetter(1))
