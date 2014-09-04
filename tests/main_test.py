0356392b2933aa7c02f89bdf588a4ec0482db4a8
Carl Winbäck <c@tunnel53.net>
2014-09-04 06:15:13 +0200

    Add a test for hamming_distance()

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from libpals.util import xor_find_singlechar_key
+from libpals.util import xor_find_singlechar_key, hamming_distance
 
 def test_xor_find_singlechar_key():
     input = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
@@ -8,3 +8,7 @@ def test_xor_find_singlechar_key():
     result = xor_find_singlechar_key(ciphertext)
     assert result['key']  == 88
     assert result['plaintext']  == b"Cooking MC's like a pound of bacon"
+
+
+def test_hamming_distance():
+    assert hamming_distance(b"this is a test", b"wokka wokka!!!") == 37
