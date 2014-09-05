cd73f0508032f7589f4457069180940063e56b20
Carl Winbäck <c@tunnel53.net>
2014-09-05 16:32:24 +0200

    Add test case for fixed_xor()

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from libpals.util import xor_find_singlechar_key, hamming_distance
+from libpals.util import xor_find_singlechar_key, hamming_distance, fixed_xor
 
 def test_xor_find_singlechar_key():
     input = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
@@ -12,3 +12,9 @@ def test_xor_find_singlechar_key():
 
 def test_hamming_distance():
     assert hamming_distance(b"this is a test", b"wokka wokka!!!") == 37
+
+
+def test_fixed_xor():
+    input = bytes.fromhex("1c0111001f010100061a024b53535009181c")
+    key = bytes.fromhex("686974207468652062756c6c277320657965")
+    assert fixed_xor(input, key) == b"the kid don't play"
