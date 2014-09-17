b5785cbd9586a767b37da2e0c71bcb1fcfed0604
Carl Winbäck <c@tunnel53.net>
2014-09-17 07:18:22 +0200

    Add test for the transpose function.

--- tests/main_test.py
+++ tests/main_test.py
@@ -3,7 +3,8 @@
 from libpals.util import (
     xor_find_singlechar_key,
     hamming_distance,
-    fixed_xor
+    fixed_xor,
+    transpose
 )
 
 def test_xor_find_singlechar_key():
@@ -22,3 +23,8 @@ def test_fixed_xor():
     input = bytes.fromhex("1c0111001f010100061a024b53535009181c")
     key = bytes.fromhex("686974207468652062756c6c277320657965")
     assert fixed_xor(input, key) == b"the kid don't play"
+
+
+def test_transpose():
+    chunks = [b'adg', b'beh', b'cfi']
+    assert transpose(chunks) == b'abcdefghi'
